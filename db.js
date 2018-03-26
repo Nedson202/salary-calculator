ae1d99f70a29c261691c9e273b1c1bbeab4a042a
Samson <simeonsamson4@gmail.com>
2018-03-26 14:37:57 +0100

    Add app database

---
+++ db.js
@@ -0,0 +1,83 @@
+const level = [
+  {
+    id: 1,
+    salary: 32000
+  },
+  {
+    id: 2,
+    salary: 54000
+  },
+  {
+    id: 3,
+    salary: 75000
+  },
+  {
+    id: 4,
+    salary: 150000
+  },
+  {
+    id: 5,
+    salary: 450000
+  }
+];
+
+const benefits = [
+  {
+    id: 1,
+    title: 'House allowance',
+    value: 50000
+  },
+  {
+    id: 2,
+    title: 'Health allowance',
+    value: 55000
+  },
+  {
+    id: 3,
+    title: 'Feeding allowance',
+    value: 25000
+  },
+  {
+    id: 4,
+    title: 'Travel allowance',
+    value: 34000
+  },
+  {
+    id: 5,
+    title: 'Transportation allowance',
+    value: 25000
+  },
+  {
+    id: 6,
+    title: 'workshop training',
+    value: 15000
+  }
+]
+
+const roles = [
+  {
+    id: 1,
+    title: 'Intern',
+    benefits: [5]
+  },
+  {
+    id: 2,
+    title: 'Associate',
+    benefits: [5, 3]
+  },
+  {
+    id: 3,
+    title: 'Managers',
+    benefits: [5, 3, 6]
+  },
+  {
+    id: 4,
+    title: 'Executives',
+    benefits: [5, 3, 6, 2]
+  },
+  {
+    id: 5,
+    title: 'Director',
+    benefits: [5, 3, 4, 2, 1, 6]
+  }
+]
